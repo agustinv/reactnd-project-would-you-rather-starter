45b398cdb54480bee2e15a7f99d66d7c973a9d3b
Agustin <agustin@edsurge.com>
2019-03-26 16:15:46 -0700

    feat: sad puppy 404 image

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function NoMatch () {
   return (
     <div className='center'>
-      <h1> 404 Page Not Found</h1>
+      <h1> 404 -- Page Not Found </h1>
+      <img src='https://media1.tenor.com/images/3e0627ac59fe94b5f6624f8c7ef5b8a5/tenor.gif?itemid=7916904' alt='sad puppy gif image'/>
     </div>
   )
 }
