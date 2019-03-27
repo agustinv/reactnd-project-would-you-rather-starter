50d874116bf819853b0c481339af7f66d214f944
Agustin <agustin@edsurge.com>
2019-03-26 18:55:17 -0700

    feat: UserScore functional component, some styling, fix: ul tags should not be wrapping divs (forgot file)

---
+++ src/components/UserScore.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function UserScore(props) {
+  const { user } = props
+  const { id, name, avatar, answered, created, score } = user
+
+  return  <div className='question'>
+            <div className='question-header'>
+              <img
+                src={avatar}
+                alt={`Avatar of ${name}`}
+                className='avatar'
+              />
+              <div className="question-title">
+                {name}
+                <span className="score"> Score: {score}</span>
+              </div>
+            </div>
+            <div className='question-info'>
+              <div>
+                <p> Answered Questions: {answered} </p>
+                <p> Created Questions: {created} </p>
+              </div>
+            </div>
+          </div>
+}
+
+export default UserScore
