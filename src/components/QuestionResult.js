adbfeae047a996b55a1c7f54d79d7110c6336113
Agustin Vilaseca <agustin@edsurge.com>
2019-03-27 23:22:02 -0700

    feat: minor css improvement to questionResults component

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function QuestionResults(props) {
   const { name, avatar, optionOneText, optionTwoText, selectedOptionOne, selectedOptionTwo, optionOneVotes, optionTwoVotes, totalVotes, percentOptionOne, percentOptionTwo } = props.results
 
+  const optOneCss = selectedOptionOne ? 'box-purple bg-purple-light' : 'box-purple'
+  const optTwoCss = selectedOptionTwo ? 'box-purple bg-purple-light' : 'box-purple'
+
   return   <div className='question'>
             <div className='question-header'>
               <img
@@ -16,15 +19,15 @@ export default function QuestionResults(props) {
             </div>
             <div className='question-info'>
               <h2>Results: </h2>
-              <div>
+              <div className={optOneCss}>
                 <h4> Would you rather {optionOneText}?</h4>
                 <p> <strong>{percentOptionOne}%</strong> of answers with <strong> ({optionOneVotes} out of {totalVotes} votes)</strong> </p>
-                <p>{selectedOptionOne ? "* you selected this answer" : ""}</p>
+                <p className='color-purple'>{selectedOptionOne ? "* you selected this answer" : ""}</p>
               </div>
-              <div>
+              <div className={optTwoCss}>
                 <h4> Would you rather {optionTwoText}?</h4>
                 <p> <strong>{percentOptionTwo}%</strong> of answers with <strong> ({optionTwoVotes} out of {totalVotes} votes)</strong> </p>
-                <p>{selectedOptionTwo ? "* you selected this answer" : ""}</p>
+                <p className='color-purple'>{selectedOptionTwo ? "* you selected this answer" : ""}</p>
               </div>
             </div>
           </div>
